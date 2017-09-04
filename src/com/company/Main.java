e376371a336e536a75053cfe6ae61797aec46a9b
zaalovich <zaalovich@icloud.com>
2017-09-04 21:19:47 +0300

    Добавляем новую программу

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("       /\\");
+        System.out.println("      /  \\");
+        System.out.println("     / || \\");
+        System.out.println("    /  ||  \\");
+        System.out.println("   /   ||   \\");
+        System.out.println("  /    ||    \\");
+        System.out.println(" /     ||     \\");
+        System.out.println("       ||       ");
+        System.out.println(" -------------- ");
+
+
+        System.out.println("$$$_____$$$$$$$$$$$$$$$_$$$_______$$$_$$$$$$$$$$");
+        System.out.println("$$$____$$$____$$$____$$$_$$$_____$$$__$$$_______");
+        System.out.println("$$$____$$$___________$$$_$$$_____$$$__$$$_______");
+        System.out.println("$$$_____$$$_________$$$___$$$___$$$___$$$$$$$$__");
+        System.out.println("$$$______$$$_______$$$_____$$$_$$$____$$$_______");
+        System.out.println("$$$_______$$$_____$$$______$$$_$$$____$$$_______");
+        System.out.println("$$$$$$$$$___$$$_$$$_________$$$$$_____$$$$$$$$$$");
+    }
+}
